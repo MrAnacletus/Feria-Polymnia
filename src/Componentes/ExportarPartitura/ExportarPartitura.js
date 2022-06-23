b4fb594a805b530475fd8489ca09698649d133f5
Sebastián Sepúlveda <sebastian.sepulvedab@sanano.usm.cl>
2022-06-22 22:16:00 -0400

    modified:   src/Componentes/ExportarPartitura/ExportarPartitura.js

--- src/Componentes/ExportarPartitura/ExportarPartitura.js
+++ src/Componentes/ExportarPartitura/ExportarPartitura.js
@@ -34,7 +34,7 @@ class ExportarPartitura extends Component {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', 'partitura.pdf');
+            link.setAttribute('download', path_a_exportar);
             document.body.appendChild(link);
             link.click();
           });
