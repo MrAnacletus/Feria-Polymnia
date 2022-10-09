ca201b5ebb772fa0e2f2aeb7b3a0180ede06db82
Daniel Magaña <daniel.magana@sansano.usm.cl>
2022-10-09 04:02:51 -0300

    Funciones para separacion de manos y cambio de tono

--- backendPython/GeneracionDePartitura/separacion_manos.py
+++ backendPython/GeneracionDePartitura/separacion_manos.py
@@ -1,52 +1,33 @@
 import music21
 from tabs import open_midi
 
-midi = open_midi("backendPython/GeneracionDePartitura/test/piano.mid")
+def derecha_piano(midi, output):
+    """Obtiene la mano derecha de un archivo midi de piano y la guarda en output
 
-#midi.show('text')
+    Args:
+        midi (string): Ruta del archivo midi
+        output (string): Ruta donde se guardara el archivo midi de la mano derecha del piano
+    """    
 
-izq = music21.stream.Score()
-der = music21.stream.Score()
-todo = music21.stream.Score()
-print(midi)
-cont = 0
-for i in midi.parts:
-    if(i.partName == "Piano"):
-        todo.append(i)
-        if(cont == 0):
-            der.append(i)
-        elif(cont == 1):
-            izq.append(i)
-            break
-        cont += 1
+    midi = open_midi(midi)
 
-for i in der.recurse():
-    #if i is note, print all note information, else, print i
-    if isinstance(i, music21.note.Note):
-        print(i.name, i.octave, i.quarterLength, i.offset, i.tie)        
-    else:
-        print(i)
+    #midi.show('text')
 
-#Falta agregarle el tempo a la mano izquierda
-#print("izq")
-#izq.show('text')
-#izq.write('midi', fp="backendPython/GeneracionDePartitura/test/test_izq.mid")
+    izq = music21.stream.Score()
+    der = music21.stream.Score()
+    todo = music21.stream.Score()
 
-#print("der")
-#der.show('text')
-der.write('midi', fp="backendPython/GeneracionDePartitura/test/test_der.mid")
+    cont = 0
+    for i in midi.parts:
+        if(i.partName == "Piano"):
+            todo.append(i)
+            if(cont == 0):
+                der.append(i)
+            elif(cont == 1):
+                izq.append(i)
+                break
+            cont += 1
 
-#todo.write('midi', fp="backendPython/GeneracionDePartitura/test/test_todo.mid")
-
-#midi.write('midi', fp="backendPython/GeneracionDePartitura/test/test_midi.mid")
-
-""" der_midi = open_midi("backendPython/GeneracionDePartitura/test/test_der.mid")
-
-print("-----------------------------------------------")
-for i in der_midi.recurse():
-    #if i is note, print all note information, else, print i
-    if isinstance(i, music21.note.Note):
-        print(i.name, i.octave, i.quarterLength, i.offset)        
-    else:
-        print(i)
- """
+    der.write('midi', fp=output)
+    
+derecha_piano("backendPython/GeneracionDePartitura/test/piano.mid", "backendPython/GeneracionDePartitura/test/test_derecha_piano.mid")
--- backendPython/GeneracionDePartitura/tono.py
+++ backendPython/GeneracionDePartitura/tono.py
@@ -1,41 +1,45 @@
 import music21
 from tabs import open_midi
 
-midi = open_midi("backendPython/GeneracionDePartitura/test/piano.mid")
-midi.show('text')
+def cambiar_tono(midi, cambio, output):
+    """Cambia el tono de la partitura midi en cambio semitonos y lo guarda en output
 
+    Args:
+        midi (string): Ruta del archivo midi
+        cambio (int): Cantidad de semitonos a cambiar, puede ir desde -11 a 11
+        output (string): Ruta donde se guardara el archivo midi con el cambio de tono
+    """    
+    midi = open_midi(midi)
+    #midi.show('text')
 
-llave=None
-for i in midi.recurse():
-    if isinstance(i, music21.key.Key):
-        llave = i
-        break
-print(llave)
+    llave=None
+    for i in midi.recurse():
+        if isinstance(i, music21.key.Key):
+            llave = i
+            break
+    #print(llave)
 
-#cambio puede ir desde -11 a 11
-cambio = 10
+    #lista de llaves musicales
+    llaves=["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
 
-#lista de llaves musicales
-llaves=["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+    nueva_llave = music21.key.Key(llaves[(llaves.index(llave.tonic.name)+cambio)%12], llave.mode)
+    #print(nueva_llave)
 
-nueva_llave = music21.key.Key(llaves[(llaves.index(llave.tonic.name)+cambio)%12], llave.mode)
-print(nueva_llave)
+    if((llaves.index(llave.tonic.name)+cambio)%12 != llaves.index(llave.tonic.name)+cambio):
+        #print("Cambio de octava")
+        if(cambio>0):
+            #subir una octava
+            midi.transpose(12, inPlace=True)
+        else:
+            #bajar una octava
+            midi.transpose(-12, inPlace=True)
 
-if((llaves.index(llave.tonic.name)+cambio)%12 != llaves.index(llave.tonic.name)+cambio):
-    print("Cambio de octava")
-    if(cambio>0):
-        #subir una octava
-        midi.transpose(12, inPlace=True)
-        pass
-    else:
-        #bajar una octava
-        midi.transpose(-12, inPlace=True)
-        pass
+    inter = music21.interval.Interval(llave.tonic, nueva_llave.tonic)
+    #print(inter)
 
-inter = music21.interval.Interval(llave.tonic, nueva_llave.tonic)
-print(inter)
+    midi_nuevo = midi.transpose(inter)
+    #midi_nuevo.show('text')
 
-midi_nuevo = midi.transpose(inter)
-#midi_nuevo.show('text')
-
-midi_nuevo.write('midi', fp="backendPython/GeneracionDePartitura/test/test_tono.mid")
+    midi_nuevo.write('midi', fp=output)
+    
+cambiar_tono("backendPython/GeneracionDePartitura/test/piano.mid", 9, "backendPython/GeneracionDePartitura/test/test_cambio_tono.mid")
