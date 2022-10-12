83d3e7ef23c9c5b1576c38cb6737f1131ddcc28f
salva <salvadordario@gmail.com>
2022-10-12 16:32:31 -0300

    modified:   backendPython/GeneracionDePartitura/main.py

--- backendPython/GeneracionDePartitura/main.py
+++ backendPython/GeneracionDePartitura/main.py
@@ -117,8 +117,15 @@ async def create_item(item: ItemEleccionInstrumentos):
     f.write(item.instrumento+"\n")
     f.close()
     if item.partitura == "no":
-      tabs.get_tab(pathname+"/no_vocals_new.mid", file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
-      d_pdf = lineas[1].strip() + '.pdf'
+      if item.instrumento == "Guitarra acústica" or item.instrumento == "Guitarra eléctrica"
+        tabs.get_tab(pathname+"/no_vocals_new.mid", file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
+        d_pdf = lineas[1].strip() + '.pdf'
+      elif item.instrumento == "Bajo":
+        tabs.get_tab(pathname+"/no_vocals_new.mid", strings = 'bass', file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
+        d_pdf = lineas[1].strip() + '.pdf'
+      else:
+        tabs.get_tab(pathname+"/no_vocals_new.mid", strings = 'ukulele', file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
+        d_pdf = lineas[1].strip() + '.pdf'
     else:
       d_pdf = try1.generar_partitura(pathname+'/no_vocals_new.mid', lineas[1].strip(), lineas[2].strip(),item.instrumento)
   else:
