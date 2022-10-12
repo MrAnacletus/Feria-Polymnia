dd85fd795eb6e73319e2c25d8eff0c01c064555d
salva <salvadordario@gmail.com>
2022-10-12 16:18:06 -0300

    modified:   backendPython/GeneracionDePartitura/try1.py

--- backendPython/GeneracionDePartitura/try1.py
+++ backendPython/GeneracionDePartitura/try1.py
@@ -1,21 +1,54 @@
-import music21
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
 import shutil
+import os
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
-def generar_partitura(path, name, autor, instrumento):
+def generar_partitura(path_to_midi, title, composer):
 
-    #print(path)
-    music21.environment.set('musescoreDirectPNGPath', '/usr/bin/mscore3')
+    path_to_pdf = title + "_temp.pdf"
 
-    parsed = music21.converter.parse(path)
-    parsed.insert(0, music21.metadata.Metadata())
-    parsed.metadata.title = name
-    parsed.metadata.composer = autor
-    parsed.metadata.lyricist = instrumento
-    fp = parsed.write('musicxml.pdf')
-    xml = str(fp).split('.')[0]+'.musicxml'
-    #name = path.split('/')[-1].split('.')[0]
-    d_pdf = './backend-js/temp/' + name + '.pdf'
-    d_xml = './backend-js/temp/' + name + '.musicxml'
-    shutil.copyfile(fp, d_pdf)
-    shutil.copyfile(xml, d_xml)
-    return name + ".pdf"
+    res = os.system("mscore3 -o " + path_to_pdf + " " + path_to_midi) 
+
+    shutil.copyfile(path_to_pdf, "temp.pdf")
+
+    packet = io.BytesIO()
+
+    # do whatever writing you want to do
+    can = canvas.Canvas(packet)
+    can.setFont('Times-Roman', 20)
+    can.drawString(250, 810, title)
+    can.setFont('Times-Roman', 12)
+    #can.drawString(30, 805, instrument)
+    can.drawString(500, 805, composer)
+    can.save()
+
+
+
+    #move to the beginning of the StringIO buffer
+    packet.seek(0)
+    new_pdf = PdfFileReader(packet)
+    # read your existing PDF
+    existing_pdf = PdfFileReader(open(path_to_pdf, "rb"))
+    output = PdfFileWriter()
+    # add the "watermark" (which is the new pdf) on the existing page
+    page = new_pdf.getPage(0)
+    page.mergePage(existing_pdf.getPage(0))
+    output.addPage(page)
+
+
+
+
+    # finally, write "output" to a real file
+    outputStream = open("./backend-js/temp/"+title+".pdf", "wb")
+    output.write(outputStream)
+    outputStream.close()
+
+    if os.path.exists(path_to_pdf):
+        os.remove(path_to_pdf)
+
+    if os.path.exists("temp.pdf"):
+        os.remove("temp.pdf")
+
+    return title + ".pdf"
