c847cc20f7fe93fda17cb69efabc5df848ef50d7
salva <salvadordario@gmail.com>
2022-10-12 21:12:16 -0300

    modified:   backendPython/GeneracionDePartitura/main.py

--- backendPython/GeneracionDePartitura/main.py
+++ backendPython/GeneracionDePartitura/main.py
@@ -31,6 +31,7 @@ class ItemEleccionInstrumentos(BaseModel):
     instrumento: str
     partitura: str
     melodia: str
+    instrumento2: str
 
 class ItemSimplificar(BaseModel):
     tono: int
@@ -117,13 +118,13 @@ async def create_item(item: ItemEleccionInstrumentos):
     f.write(item.instrumento+"\n")
     f.close()
     if item.partitura == "no":
-      if item.instrumento == "Guitarra acústica" or item.instrumento == "Guitarra eléctrica":
+      if item.instrumento2 == "Guitarra acústica" or item.instrumento2 == "Guitarra eléctrica":
         tabs.get_tab(pathname+"/no_vocals_new.mid", file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
         d_pdf = lineas[1].strip() + '.pdf'
-      elif item.instrumento == "Bajo":
+      elif item.instrumento2 == "Bajo":
         tabs.get_tab(pathname+"/no_vocals_new.mid", strings = 'bass', file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
         d_pdf = lineas[1].strip() + '.pdf'
-      else:
+      elif item.instrumento2 == "Ukelele":
         tabs.get_tab(pathname+"/no_vocals_new.mid", strings = 'ukulele', file_path='./backend-js/temp/' + lineas[1].strip() + '.pdf',generate_file=True,author=lineas[2].strip(),title=lineas[1].strip(),instrument=item.instrumento, max_lenght=70)
         d_pdf = lineas[1].strip() + '.pdf'
     else:
