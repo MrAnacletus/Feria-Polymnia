257e7047a2a5430f4bcedc2e81b3a8696d264550
Sebastian <sebastian.sepulvedab@sansano.usm.cl>
2022-11-15 17:35:59 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component{
 	}
 	RenderPage(){
 		let render;
-		render = <EleccionInicial sendData={this.changePage} boolean = {boolean} seleccionarInstrumento={this.seleccionarInstrumento}></EleccionInicial>;
+		render = <Menu sendData={this.changePage}></Menu>;
 		if (this.state){
 			if (this.state.toRender === "Menu"){
 				render = <Menu sendData={this.changePage}></Menu>;
